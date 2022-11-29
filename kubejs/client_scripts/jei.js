6045f8cf608edfde0fbe5215254c2d8b94d5a13c
Nesios <52352139+N3siOS@users.noreply.github.com>
2022-11-29 15:23:47 +0100

    remove hidden diamond heart in JEI

--- kubejs/client_scripts/jei.js
+++ kubejs/client_scripts/jei.js
@@ -47,7 +47,6 @@ onEvent('jei.hide.items', event => {
         'quark:pickarang',
         'quark:dragon_scale',
         'quark:chute',
-        'quark:diamond_heart',
         'quark:white_candle',
         'quark:orange_candle',
         'quark:magenta_candle',
